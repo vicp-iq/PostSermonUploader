fa017cc96cf1d2192f10e6a9495080fcca439171
Vic Peters <sandro997@gmail.com>
2018-09-29 19:48:58 -0500

    Cleaned up some warnings

--- src/PostSermonUploader/Models/Attachment.cs
+++ src/PostSermonUploader/Models/Attachment.cs
@@ -5,12 +5,6 @@
         public string Name { get; set; }
         public string Path { get; set; }
 
-        public string DisplayText
-        {
-            get
-            {
-                return string.Format("Attachment: {0} - {1}", Name, Path);
-            }
-        }
+        public string DisplayText => $"Attachment: {Name} - {Path}";
     }
 }
